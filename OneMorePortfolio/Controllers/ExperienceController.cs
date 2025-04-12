0d2a2be67f2193d9557c86979ac4c3f50832fee7
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-12 21:47:41 +0300

    s

--- OneMorePortfolio/Controllers/ExperienceController.cs
+++ OneMorePortfolio/Controllers/ExperienceController.cs
@@ -1,12 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortolioUdemy.DAL.Entities;
+using OneMorePortfolio.Dal.Context;
 
 namespace OneMorePortfolio.Controllers
 {
     public class ExperienceController : Controller
     {
+        OneMorePortfolioContext context=new OneMorePortfolioContext();
         public IActionResult ExperienceList()
+        {
+            var experiences = context.Experiences.ToList();
+            return View(experiences);
+        }
+        [HttpGet]
+        public IActionResult CreateExperience()
         {
             return View();
         }
+        [HttpPost]
+        
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList", "Experience");  
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var val=context.Experiences.Find(id);
+            context.Experiences.Remove(val);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList", "Experience");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var val = context.Experiences.Find(id);
+            return View(val);
+        }
+        [HttpPost]
+
+        public IActionResult UpdateExperience(Experience exp)
+        {
+          
+            context.Experiences.Update(exp);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList", "Experience");
+        }
+
     }
 }
---
+++ OneMorePortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneMorePortfolio.Dal.Context;
+
+namespace OneMorePortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        OneMorePortfolioContext context = new OneMorePortfolioContext();
+        public IActionResult Index()
+        {
+            ViewBag.v1 = context.Skills.Count();
+            ViewBag.v2 = context.Messages.Count();
+            ViewBag.v3 = context.Messages.Where(x => x.IsRead == false).Count();
+            ViewBag.v4 = context.Messages.Where(x => x.IsRead == true).Count();
+            
+            return View();
+        }
+    }
+}
