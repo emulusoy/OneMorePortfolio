9b6580a62455f9b4f47d3fd48564e531f6b850fc
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-12 10:58:35 +0300

    Add project files.

---
+++ OneMorePortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OneMorePortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
