3e21627c3d6bf24befe0d631af5b87570b22e7d1
Mustafa Ulusoy <116921446+emulusoy@users.noreply.github.com>
2025-04-12 15:06:55 +0300

    .

---
+++ OneMorePortfolio/ViewComponents/LayoutViewComponents/_LayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OneMorePortfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutHeadComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
---
+++ OneMorePortfolio/ViewComponents/LayoutViewComponents/_LayoutNavbarComponentPartial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OneMorePortfolio.Dal.Context;
+
+
+namespace OneMorePortfolio.ViewComponents.LayoutViewComponents
+{
+	public class _LayoutNavbarComponentPartial:ViewComponent
+	{
+		OneMorePortfolioContext context = new OneMorePortfolioContext();
+		public IViewComponentResult Invoke()
+		{
+			ViewBag.toDoListCount = context.ToDoLists.Where(x => x.Status == false).Count();
+			var values = context.ToDoLists.Where(x => x.Status == false).ToList();
+			return View(values);
+		}
+	}
+}
